7bd2005e4860144c61db5d3507f86ab3466b6248
Waltermelion <guilhermelpmeneses@gmail.com>
2022-03-03 12:36:09 +0000

    ex-19-21

--- proj_1/Assets/02-Scripts/TestTransformations.cs
+++ proj_1/Assets/02-Scripts/TestTransformations.cs
@@ -7,13 +7,13 @@ public class TestTransformations : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        transform.Rotate(0, 90, 0);
+        transform.Translate(5, -1, 0);
+        
+
         Debug.Log(transform.localPosition);
-        Debug.Log(transform.localEulerAngles);
-        Debug.Log(transform.localScale);
 
         Debug.Log(transform.position);
-        Debug.Log(transform.eulerAngles);
-        Debug.Log(transform.lossyScale);
     }
 
     // Update is called once per frame
