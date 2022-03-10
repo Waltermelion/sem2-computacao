681b880aaca25c63c254de91e5b9c67c3effa204
Waltermelion <guilhermelpmeneses@gmail.com>
2022-03-10 13:44:50 +0000

    Light goes yellow pingPong, needs updating (ex 6)

--- proj_1/Assets/02-Scripts/DayLightCycle.cs
+++ proj_1/Assets/02-Scripts/DayLightCycle.cs
@@ -2,11 +2,36 @@ using UnityEngine;
 
 public class DayLightCycle : MonoBehaviour
 { 
-    //[SerializeField] private float RotationSpeed;
     [SerializeField] private float StartingXRotation;
+    [SerializeField] private bool redLimit = false;
     void Update()
     {
-        //StartingXRotation += RotationSpeed;
         transform.Rotate(StartingXRotation, 0, 0);
+
+        Color color1 = this.GetComponent<Light>().color;
+
+        color1 = Color.Lerp(Color.white, Color.yellow, Mathf.PingPong(Time.time, 1));
+
+        /*if (redLimit)
+        {
+            if (color1.r >= 1)
+            {
+                color1.r -= 0.001f;
+            }
+        }
+        else
+        {
+            color1.r += 0.001f;
+        }
+
+        if(transform.localEulerAngles.x <= 0.1 || transform.localEulerAngles.x >= 20)
+        {
+            redLimit = true;
+        }else if(transform.localEulerAngles.x >= 0.1 || transform.localEulerAngles.x <= 20)
+        {
+            redLimit = false;
+        }*/
+
+        this.GetComponent<Light>().color = color1;
     }
 }
